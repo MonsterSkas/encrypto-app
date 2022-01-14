035a6557554fc11234ca46833c2cfe1c11ab4ddc
Spandan Dey <Skyperflames@gmail.com>
2022-01-14 13:53:56 +0530

    initial ccommit

---
+++ include/Headers.h
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <Windows.h>
---
+++ src/Main.cpp
@@ -0,0 +1,15 @@
+#include <Headers.h>
+
+int main()
+{
+    SetConsoleTitleA("Encrypto v1.0");
+    system("color e");
+
+    std::cout << "| Encrypto v1.0";
+
+    std::cout << "\n\n| 1 |Encrypt";
+    std::cout << "\n| 2 |Decrypt";
+
+    std::cout << "\n\n> ";
+    _getch();
+}
